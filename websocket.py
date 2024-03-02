a89193ee1a2c63aeefbcd35774a25bed13fd5e21
Ned <iamcalledned@gmail.com>
2024-03-02 11:04:26 -0500

    Update websocket.py

--- websocket.py
+++ websocket.py
@@ -10,7 +10,7 @@ from starlette.endpoints import WebSocketEndpoint
 
 from openai_utils_generate_answer import generate_answer
 from config import Config
-from chat_bot_database import create_db_pool, get_user_info_by_session_id, save_recipe_to_db, clear_user_session_id, get_user_id
+from chat_bot_database import create_db_pool, get_user_info_by_session_id, save_recipe_to_db, clear_user_session_id, get_user_id, favorite_recipe, get_recipe_for_printing, get_saved_recipes_for_user, un_favorite_recipe, get_recent_messages, get_messages_before
 from process_recipe import process_recipe
 from fastapi import APIRouter
 from fastapi import Request
@@ -21,6 +21,9 @@ from get_recipe_card import get_recipe_card
 
 import spacy
 import re
+from starlette.websockets import WebSocket
+from datetime import datetime
+import httpx
 
 # Initialize Redis client
 redis_client = redis.Redis(host='localhost', port=6379, db=0)
@@ -34,7 +37,7 @@ connections = {}
 tasks = {}  # Dictionary to track scheduled tasks for session cleanup
 
 log_file_path = Config.LOG_PATH
-LOG_FORMAT = 'generate-answer - %(asctime)s - %(processName)s - %(name)s - %(levelname)s - %(message)s'
+LOG_FORMAT = 'WEBSOCKET - %(asctime)s - %(processName)s - %(name)s - %(levelname)s - %(message)s'
 
 logging.basicConfig(
     filename=log_file_path,
@@ -49,6 +52,7 @@ async def create_pool():
 
 @router.post("/logout")
 async def logout(request: Request):
+    
     session_id = request.json().get('session_id', '')
     if session_id:
         # Remove session data from Redis
@@ -66,28 +70,44 @@ async def startup_event():
     app.state.pool = await create_pool()
     print("Database pool created")
 
+    print("connections at startup:", connections)
+
 # Function to schedule session data cleanup
 async def clear_session_data_after_timeout(session_id, username):
     try:
-        await asyncio.sleep(600)  # Adjust the timeout as needed
-        redis_client.delete(session_id)
-        await clear_user_session_id(app.state.pool, session_id)
-        print(f"Session data cleared for user {username}")
+        await asyncio.sleep(3600)  # wait an hour before cleaning
+        # Check if the session still exists before clearing
+        if redis_client.exists(session_id):
+            redis_client.delete(session_id)
+            await clear_user_session_id(app.state.pool, session_id)
+            print(f"Session data cleared for user {username}")
+
+            # Send a WebSocket message to the client to log out
+            if username in connections:
+                websocket = connections[username]
+                await websocket.send_text(json.dumps({'action': 'force_logout'}))
     except Exception as e:
         print(f"Error in session cleanup task for {username}: {e}")
 
-
-@app.websocket("/ws")
+@app.websocket("/")
 async def websocket_endpoint(websocket: WebSocket):
-    print('hit websocket')
     await websocket.accept()
+    cookies = websocket.cookies
+    session_id_from_cookies = cookies.get('session_id')
+        # Obtain client IP address
+    client_host, client_port = websocket.client
+    client_ip = client_host
+    print(f"Client IP: {client_ip}")
+    print("connections at websocket_endpoint:", connections)
+    
+    
     username = None
     
     async def ping_client():
         while True:
             try:
                 await websocket.send_text(json.dumps({'action': 'ping'}))
-                await asyncio.sleep(10)  # Send a ping every 30 seconds
+                await asyncio.sleep(30)  # Send a ping every 30 seconds
             except Exception as e:
                 print(f"Error sending ping: {e}")
                 break
@@ -97,126 +117,145 @@ async def websocket_endpoint(websocket: WebSocket):
     try:
         initial_data = await websocket.receive_text()
         initial_data = json.loads(initial_data)
-        session_id = initial_data.get('session_id', '')
-        print("session_id from get session_id:", session_id)
+        #session_id = initial_data.get('session_id', '')
+        session_id = session_id_from_cookies
+    
 
         if session_id:
             session_data = redis_client.get(session_id)
-            print("session_data from redis:", session_data)
+            
             if session_data:
                 session_data = json.loads(session_data)
                 username = session_data['username']
-                print("username from redis:", username)
-
                 # Renew the session expiry time upon successful connection
                 redis_client.expire(session_id, 3600)  # Reset expiry to another hour
+                #get and send recent messages
+                userID = await get_user_id(app.state.pool, username)
+                
+                recent_messages = await get_recent_messages(app.state.pool, userID)
+                await websocket.send_text(json.dumps({
+                        'action': 'recent_messages',
+                        'messages': recent_messages
+                        }))
+    
             else:
-                await websocket.send_text(json.dumps({'error': 'Invalid session'}))
+                await websocket.send_text(json.dumps({'action': 'redirect_login', 'error': 'Invalid session'}))
+                #await websocket.send_text(json.dumps({'error': 'Invalid session'}))
                 return
         else:
-            await websocket.send_text(json.dumps({'error': 'Session ID required'}))
+            await websocket.send_text(json.dumps({'action': 'redirect_login', 'error': 'Session ID required'}))
+            #await websocket.send_text(json.dumps({'error': 'Session ID required'}))
             return
 
         while True:
             data = await websocket.receive_text()
             data_dict = json.loads(data)
-            print("data_dict from receive_text:", data_dict)
-                       
-            if data_dict.get('action') == 'pong':
-                print("Pong received from client")
-                redis_client.expire(session_id, 3600)  # Reset expiry to another hour
-                print("extended redis via ping pong")
-                continue
+            message = data_dict.get('message', '')
+            #session_id = data_dict.get('session_id', '')
+            session_id = session_id_from_cookies
+            
+                # Validate session_id
+            if not session_id or not redis_client.exists(session_id):
+                await websocket.send_text(json.dumps({'action': 'redirect_login', 'error': 'Invalid or expired session'}))
+                
 
-            # Renew the session expiry time after receiving each message
-            redis_client.expire(session_id, 3600)  # Reset expiry to another hour
-            print("extended redis")
+            # Renew the session expiry time
+            redis_client.expire(session_id, 3600)
+            
+            
+            if data_dict.get('action') == 'pong':
+                redis_client.expire(session_id, 3600)  # Reset expiry to another hour
+                continue
 
             if 'action' in data_dict and data_dict['action'] == 'save_recipe':
                 # Handle the save recipe action
-                recipe_text = data_dict['content']
-                #recipe_text = recipe_text.replace("\n", " ").replace("\t", " ")
-                # Regular expression for extracting title, servings, and times
-                title_match = re.search(r"A recipe for: (.+?)\n", recipe_text)
-                servings_match = re.search(r"Servings: (.+?)\n", recipe_text)
-                prep_time_match = re.search(r"Prep time: (.+?)\n", recipe_text)
-                cook_time_match = re.search(r"Cook time: (.+?)\n", recipe_text)
-                total_time_match = re.search(r"Total time: (.+?)\n", recipe_text)
-
-                # Ingredient extraction
-                ingredients_match = re.search(r"Ingredients:\n(.*?)\n\nInstructions:", recipe_text, re.DOTALL)
-                if ingredients_match:
-                    ingredients_text = ingredients_match.group(1)
-                    ingredients_lines = [line.strip() for line in ingredients_text.split('\n') if line.strip()]
-                    ingredients = []
-                    current_category = None
-                    for line in ingredients_lines:
-                        if line.endswith(':'):
-                            current_category = line[:-1]
-                        else:
-                            ingredient = {'item': line, 'category': current_category}
-                            ingredients.append(ingredient)
-                else:
-                    ingredients = []  # or handle the error appropriately
-
-                # Instruction extraction
-                instructions_section_match = re.search(r"(Instructions|Directions):\n", recipe_text)
-                if instructions_section_match:
-                    # Find the start index of instructions
-                    start_index = instructions_section_match.end()
-                    
-                    # Extract text starting from 'Instructions' or 'Directions'
-                    following_text = recipe_text[start_index:]
-
-                    # Use regex to find all lines starting with a number
-                    instructions = re.findall(r"^\d+\.\s*(.+)$", following_text, re.MULTILINE)
-                else:
-                    instructions = []  # Handle the case where instructions are not found
-
-                # Structured recipe data
-                recipe_data = {
-                    # ... other fields ...
-                    "instructions": instructions
-                }
-
-                # Structured recipe data
-                recipe_data = {
-                    "title": title_match.group(1) if title_match else None,
-                    "servings": servings_match.group(1) if servings_match else None,
-                    "prep_time": prep_time_match.group(1) if prep_time_match else None,
-                    "cook_time": cook_time_match.group(1) if cook_time_match else None,
-                    "total_time": total_time_match.group(1) if total_time_match else None,
-                    "ingredients": ingredients,
-                    "instructions": instructions
-                }
-
-                
-
                 
                 # Initialize save_result with a default value
                 save_result = 'not processed'  # You can set a default value that makes sense for your application  
                 userID = await get_user_id(app.state.pool, username)
-                print("userID from new get user:", userID)
-                print("recipe data", recipe_data)
-                save_result = await save_recipe_to_db(app.state.pool, userID, recipe_data)                
-                print("save result:", save_result)
+                recipe_id = data_dict.get('content')
+    
+                save_result = await favorite_recipe(app.state.pool, userID, recipe_id)
+    
+
                 if save_result == 'Success':
                    save_result = 'success'
-                   print("save result:", save_result)
+                   
+    
                 await websocket.send_text(json.dumps({'action': 'recipe_saved', 'status': save_result}))
-            else:
+                continue
+            
+            if 'action' in data_dict and data_dict['action'] == 'get_user_recipes':
+    
+                user_id = await get_user_id(app.state.pool, username)
+    
+                if user_id:
+                    saved_recipes = await get_saved_recipes_for_user(app.state.pool, user_id)
+    
+                    await websocket.send_text(json.dumps({
+                        'action': 'user_recipes_list',
+                        'recipes': saved_recipes
+                    }))
+                continue
+
+
+            if 'action' in data_dict and data_dict['action'] == 'load_more_messages': 
+                userID = await get_user_id(app.state.pool, username)
+                last_loaded_timestamp = data_dict.get('last_loaded_timestamp')
+                older_messages = await get_messages_before(app.state.pool, userID, last_loaded_timestamp)
+                await websocket.send_text(json.dumps({
+                        'action': 'older_messages',
+                        'messages': older_messages
+                        }))
+
+
+            if 'action' in data_dict and data_dict['action'] == 'print_recipe':
+                # Handle the print_recipe recipe action
+                
+                # Initialize save_result with a default value
+                
+                recipe_id = data_dict.get('content')
+    
+                print_result = await get_recipe_for_printing(app.state.pool, recipe_id)
+    
+
+                await websocket.send_text(json.dumps({'action': 'recipe_printed', 'data': print_result}))
+                continue
+            
+            if 'action' in data_dict and data_dict['action'] == 'remove_recipe':
+                # Handle removing favorite_recipe
+                
+                recipe_id = data_dict.get('content')
+                
+                userID = await get_user_id(app.state.pool, username)
+                
+                remove_result = await un_favorite_recipe(app.state.pool, userID, recipe_id)
+                
+                
+
+                #await websocket.send_text(json.dumps({'action': 'recipe_printed', 'data': print_result}))
+                continue
+            
+            if 'action' in data_dict and data_dict['action'] == 'chat_message':
                 # Handle regular messages
                 message = data_dict.get('message', '')
-                user_ip = "User IP"  # Placeholder for user IP
+                
                 uuid = str(uuid4())
-
-                response_text, content_type = await generate_answer(app.state.pool, username, message, user_ip, uuid)
+                
+                
+                user_ip = client_ip
+                print(f"User IP: {user_ip}")
+                response_text, content_type, recipe_id = await generate_answer(app.state.pool, username, message, user_ip, uuid)
                 response = {
                     'response': response_text,
                     'type': content_type,
+                    'recipe_id': recipe_id
                 }
                 
                 await websocket.send_text(json.dumps(response))
+                continue
+            else:
+                print("Invalid action:", data_dict)
 
     except WebSocketDisconnect:
         print(f"WebSocket disconnected for user {username}")
