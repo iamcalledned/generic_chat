2fe4ff19fcd58146dc9de692d72e07d1ff0f3e02
Ned <iamcalledned@gmail.com>
2024-05-29 14:41:11 -0400

    fix for missing persona

--- websocket.py
+++ websocket.py
@@ -236,7 +236,6 @@ async def websocket_endpoint(websocket: WebSocket):
             if 'action' in data_dict and data_dict['action'] == 'clear_conversations': 
                 userID = await get_user_id(app.state.pool, username)
                 print('userID:', userID)
-                persona = data_dict.get('persona')
                 print('persona:', persona)
                 active_thread = await get_active_thread_for_delete(app.state.pool, userID, persona)
                 print('active thread:', active_thread)
