4ca2ca26f63b4fa9eaa6c2a5f6f3041bcf10fc25
Ned <iamcalledned@gmail.com>
2024-06-21 11:54:13 -0400

    Update chat_message.py

--- handlers/chat_message.py
+++ handlers/chat_message.py
@@ -6,6 +6,7 @@ from utilities import format_response
 async def handle_chat_message(websocket, data_dict, pool, username, client_ip, persona):
     message = data_dict.get('message', '')
     uuid = str(uuid4())
+    print("persona from handle_chat_message:", persona)
     response_json, content_type, recipe_id = await generate_answer(pool, username, message, client_ip, uuid, persona)
     response_text = format_response(response_json, content_type)
     response = {
