9e5f89ae263e6027d83d45bae1c427e928803d64
Ned <iamcalledned@gmail.com>
2024-06-21 12:57:42 -0400

    format recent messages, maybe

--- handlers/select_persona.py
+++ handlers/select_persona.py
@@ -1,5 +1,6 @@
 import json
 from db_functions import get_user_id, get_active_thread_for_user, get_recent_messages
+from utilities import format_response
 
 async def handle_select_persona(websocket, data_dict, pool, username):
     persona = data_dict.get('persona')
@@ -8,7 +9,8 @@ async def handle_select_persona(websocket, data_dict, pool, username):
     if active_thread:
         threadID = active_thread['ThreadID']
         recent_messages = await get_recent_messages(pool, userID, persona, threadID)
+        formatted_messages = await format_response(recent_messages)
         await websocket.send_text(json.dumps({
             'action': 'recent_messages',
-            'messages': recent_messages
+            'messages': formatted_messages
         }))
