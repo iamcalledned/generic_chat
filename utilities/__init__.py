f39310f90092c84e820c25b077775cae8f6b69e3
Ned <iamcalledned@gmail.com>
2024-06-24 09:53:41 -0400

    Update __init__.py

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -2,3 +2,4 @@
 from .session_management import clear_session_data_after_timeout, verify_session_id
 from .format_response import format_response
 from .process_content import process_message_content
+from .format_response_table import format_response_table
