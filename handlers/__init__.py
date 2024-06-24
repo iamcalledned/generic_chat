c03a9fb213eccbe36523bab8a309113f66814afd
Ned <iamcalledned@gmail.com>
2024-06-24 08:16:15 -0400

    small fix need to include in init

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -5,3 +5,4 @@ from .delete_selected_threads import handle_delete_selected_threads
 from .load_more_messages import handle_load_more_messages
 from .pong import handle_pong
 from .select_persona import handle_select_persona
+from .process_content import process_conent
